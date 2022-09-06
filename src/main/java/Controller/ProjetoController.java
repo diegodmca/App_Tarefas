07a9bc7ea91b36d837a360335b74ff953a26191c
Diego Carvalho (diegodmca) <diegodmca@outlook.com>
2022-09-06 17:21:49 -0300

    Aprimoramento dos controllers

---
+++ src/main/java/Controller/ProjetoController.java
@@ -0,0 +1,119 @@
+
+package Controller;
+
+import Model.Projetos;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import util.credenciaisBd;
+
+/**
+ *
+ * @author diegocarvalho
+ */
+public class ProjetoController {
+    
+    public void salvar (Projetos projetos){
+        
+        String sql = "INSERT INTO projects (name, description, "
+                + "createdAt, "
+                + "updatedAt) VALUES(?, ?, ?, ?)";
+        
+        Connection connect = null;
+        PreparedStatement statement = null;
+        
+        try {
+            connect = credenciaisBd.getConnection();
+            statement = connect.prepareStatement(sql);
+            statement.setString(1, projetos.getNome());
+            statement.setString(2, projetos.getDescricao());
+            statement.setDate(3, new Date(projetos.getCriadoEm().getTime()));
+            statement.setDate(4, new Date(projetos.getAtualizadoEm().getTime()));
+            statement.execute();
+            
+        }catch (Exception ex) {
+            
+            throw new RuntimeException("Erro ao salvar o projeto", ex);
+            
+        }finally {
+            credenciaisBd.closeConnection(connect, statement);
+        }
+    }
+    public void atualizar(Projetos projetos){
+        
+        String sql = "UPDATE projects SET name = ?, description = ?, createdAt = ?, updatedAt = ? WHERE id = ?";
+        
+        Connection connect = null;
+        PreparedStatement statement = null;
+        
+        try {
+            connect = credenciaisBd.getConnection();
+            statement = connect.prepareStatement(sql);
+            statement.setString(1, projetos.getNome());
+            statement.setString(2, projetos.getDescricao());
+            statement.setDate(3, new Date(projetos.getCriadoEm().getTime()));
+            statement.setDate(4, new Date(projetos.getAtualizadoEm().getTime()));
+            
+        }catch (Exception ex) {
+            throw new RuntimeException("Erro ao atualizar as informacoes", ex);
+            
+        }finally {
+            credenciaisBd.closeConnection(connect, statement);
+        }
+    }
+    public void apagarPorId(int projetoId){
+        
+          String sql = "DELETE FROM projects WHERE id = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            connection = credenciaisBd.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, projetoId);
+            statement.execute();
+            
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao deletar a tarefa", ex);
+            
+        }finally {
+            credenciaisBd.closeConnection(connection, statement);
+        }
+    }
+    public List<Projetos> todosProjetos(){
+       String sql = "SELECT * FROM projects";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        //Lista de projetos que sera devolvida
+        List<Projetos> listaProjetos = new ArrayList<Projetos>();
+        
+        try {
+            connection = credenciaisBd.getConnection();
+            statement = connection.prepareStatement(sql);
+            
+            //valor retornado pela execucao da query
+            resultSet = statement.executeQuery();
+            //enquanto houver valores, percorrer
+            while(resultSet.next()){
+                Projetos projeto = new Projetos();
+                
+                projeto.setNome(resultSet.getString("name"));
+                projeto.setDescricao(resultSet.getString("description"));
+                projeto.setCriadoEm(resultSet.getDate("createdAt"));
+                projeto.setAtualizadoEm(resultSet.getDate("updatedAt"));
+                
+                listaProjetos.add(projeto);
+            }
+        }catch(Exception ex) {
+            throw new RuntimeException("Erro ao atualizar a lista de projetos", ex);
+        }
+        return listaProjetos;
+            }
+}
--- src/main/java/Controller/TarefaController.java
+++ src/main/java/Controller/TarefaController.java
@@ -6,7 +6,9 @@ import java.sql.Connection;
 import Model.Tarefas;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import util.credenciaisBd;
 
@@ -19,7 +21,7 @@ public class TarefaController {
     
     public void salvar(Tarefas tarefa){
         
-       String sql = "INSERT INTO tasks ('idProject', 'name', description, "
+       String sql = "INSERT INTO tasks (idProject, name, description, "
                + "completed, "
                + "notes, "
                + "deadline, "
@@ -54,7 +56,7 @@ public class TarefaController {
     public void atualiza(Tarefas tarefa){
         
        String sql = "UPDATE tasks SET idProject = ?, name = ?, "
-               + "description, "
+               + "description = ?, "
                + "notes = ?, "
                + "completed = ?, "
                + "deadline = ?,"
@@ -76,6 +78,7 @@ public class TarefaController {
            statement.setDate(6, new Date(tarefa.getPrazo().getTime()));
            statement.setDate(7, new Date(tarefa.getCriadoEm().getTime()));
            statement.setDate(8, new Date(tarefa.getAtualizadoEm().getTime()));
+           statement.setInt(9, tarefa.getId());
            statement.execute();
            
        }catch (Exception ex) {
@@ -109,6 +112,44 @@ public class TarefaController {
     
     public List<Tarefas> todasTarefas(int projetoId){
         
-        return null;
+        String sql = "SELECT * FROM tasks WHERE idproject = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        //Lista de tarefas que sera devolvida
+        List<Tarefas> listaTarefas = new ArrayList<Tarefas>();
+        
+        try {
+            connection = credenciaisBd.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, projetoId);
+            //valor retornado pela execucao da query
+            resultSet = statement.executeQuery();
+            //enquanto houver valores, percorrer
+            while(resultSet.next()){
+                Tarefas tarefa = new Tarefas();
+                
+                tarefa.setId(resultSet.getInt("id"));
+                tarefa.setIdProjeto(resultSet.getInt("idproject"));
+                tarefa.setNome(resultSet.getString("name"));
+                tarefa.setDescricao(resultSet.getString("description"));
+                tarefa.setAnotacoes(resultSet.getString("notes"));
+                tarefa.setConcluido(resultSet.getBoolean("completed"));
+                tarefa.setPrazo(resultSet.getDate("deadline"));
+                tarefa.setCriadoEm(resultSet.getDate("createdAt"));
+                tarefa.setAtualizadoEm(resultSet.getDate("updatedAt"));
+                
+                listaTarefas.add(tarefa);
+            }
+        }catch(Exception ex) {
+            
+            throw new RuntimeException("Erro ao capturar as informacoes", ex);
+            
+        }finally {
+            credenciaisBd.closeConnection(connection, statement);
+            
+        }
+        return listaTarefas;
     }
 }
